01f5792f0fcf4c64e4e6a01e33ee3ba361c25e0b
NikitaPST <nikita.pestin@gmail.com>
2024-08-31 02:36:31 +0200

    window initialization

--- project/WellBakedEngineLib/include/engine.h
+++ project/WellBakedEngineLib/include/engine.h
@@ -1,6 +1,8 @@
 #ifndef _ENGINE_H
 #define _ENGINE_H
 
+#include <Windows.h>
+
 #include "input.h"
 #include "graphics.h"
 
@@ -16,10 +18,24 @@ namespace WBEngine
 		void Shutdown();
 		void Run();
 
+		LRESULT CALLBACK MessageHandler(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
 	private:
 		Graphics* m_pGraphics;
 		Input* m_pInput;
+
+		HINSTANCE m_hInstance;
+		HWND m_hWnd;
+
+		bool InitializeWindow(int& nWidth, int& nHeight);
+		bool FrameProcessing();
 	};
+
+	// Global functions
+	static LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	// Global handle
+	static Engine* g_pEngineInstance = nullptr;
 }
 
 #endif
--- project/WellBakedEngineLib/src/engine.cpp
+++ project/WellBakedEngineLib/src/engine.cpp
@@ -1,7 +1,6 @@
 #include "engine.h"
 
 #include <sstream>
-#include <Windows.h>
 
 #include "logger.h"
 #include "configuration/iniconfiguration.h"
@@ -10,8 +9,13 @@ namespace WBEngine
 {
 	Engine::Engine()
 	{
+		m_hInstance = NULL;
+		m_hWnd = NULL;
+
 		m_pGraphics = new Graphics();
 		m_pInput = new Input();
+
+		g_pEngineInstance = this;
 	}
 
 	Engine::~Engine()
@@ -27,6 +31,8 @@ namespace WBEngine
 			delete m_pGraphics;
 			m_pGraphics = nullptr;
 		}
+
+		g_pEngineInstance = nullptr;
 	}
 
 	bool Engine::Initialize()
@@ -44,8 +50,15 @@ namespace WBEngine
 		sLogMessage << L"\tHeight: " << nHeight;
 		Logger::Info(sLogMessage.str());
 
+		if (!InitializeWindow(nWidth, nHeight))
+		{
+			Logger::Error(L"Windows initialization failed");
+			return false;
+		}
+
 		if (!m_pGraphics->Initialize())
 		{
+			Logger::Error(L"Graphics initialization failed");
 			return false;
 		}
 
@@ -62,6 +75,13 @@ namespace WBEngine
 			m_pGraphics->Shutdown();
 		}
 
+		if (m_hWnd)
+		{
+			DestroyWindow(m_hWnd);
+			m_hWnd = NULL;
+		}
+		UnregisterClass(L"Well-Baked Engine", m_hInstance);
+
 		Logger::Info(L"Engine shutdown finished");
 	}
 
@@ -69,6 +89,114 @@ namespace WBEngine
 	{
 		Logger::Info(L"Engine is running");
 
-		MessageBox(NULL, L"Well-Baked Engine is running", L"Well-Baked Engine", MB_ICONINFORMATION | MB_OK);
+		MSG msg;
+		ZeroMemory(&msg, sizeof(MSG));
+
+		bool bDone = false;
+		while (!bDone)
+		{
+			if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+
+			if (msg.message == WM_QUIT)
+			{
+				bDone = true;
+			}
+			else
+			{
+				if (!FrameProcessing())
+				{
+					Logger::Error(L"Frame processing failed");
+					bDone = true;
+				}
+			}
+		}
+	}
+
+	LRESULT CALLBACK Engine::MessageHandler(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		switch (uMsg)
+		{
+			case WM_CLOSE:
+				DestroyWindow(hWnd);
+				return 0;
+			case WM_DESTROY:
+				PostQuitMessage(0);
+				return 0;
+			default:
+				return DefWindowProc(hWnd, uMsg, wParam, lParam);
+		}
+	}
+
+	bool Engine::InitializeWindow(int& nWidth, int& nHeight)
+	{
+		Logger::Info(L"Windows initialization started");
+
+		m_hInstance = GetModuleHandle(NULL);
+
+		WNDCLASSEX wc;
+		ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+		wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+		wc.lpfnWndProc = WndProc;
+		wc.cbClsExtra = 0;
+		wc.cbWndExtra = 0;
+		wc.hInstance = m_hInstance;
+		wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+		wc.hIcon = wc.hIcon;
+		wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+		wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+		wc.lpszMenuName = NULL;
+		wc.lpszClassName = L"Well-Baked Engine";
+		wc.cbSize = sizeof(WNDCLASSEX);
+
+		if (RegisterClassEx(&wc) == 0)
+		{
+			Logger::Error(L"Failed to register windows class");
+			return false;
+		}
+
+		int nPosX = 100, nPosY = 50;
+
+		m_hWnd = CreateWindowEx(
+			WS_EX_APPWINDOW,
+			L"Well-Baked Engine",
+			L"Well-Baked Engine",
+			WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_BORDER,
+			nPosX,
+			nPosY,
+			nWidth,
+			nHeight,
+			NULL,
+			NULL,
+			m_hInstance,
+			NULL
+		);
+		
+		if (m_hWnd == NULL)
+		{
+			Logger::Error(L"Failed to create window");
+			return false;
+		}
+
+		ShowWindow(m_hWnd, SW_SHOW);
+		SetForegroundWindow(m_hWnd);
+		SetFocus(m_hWnd);
+
+		Logger::Info(L"Windows initialization finished");
+		return true;
+	}
+
+	bool Engine::FrameProcessing()
+	{
+		return true;
+	}
+
+	LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		return g_pEngineInstance->MessageHandler(hWnd, uMsg, wParam, lParam);
 	}
 }
